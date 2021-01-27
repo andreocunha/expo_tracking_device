f7e26c416a2679f60748186c8f4a5efea2c3bc5e
andreocunha <andreoliveiracunha20@gmail.com>
2021-01-27 15:17:58 -0300

    mobile

--- App.js
+++ App.js
@@ -1,15 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Device from 'expo-device';
+import * as Location from 'expo-location';
+import io from "socket.io-client";
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [expoPushToken, setExpoPushToken] = useState('teste');
+  // const socket = io.connect('http://192.168.15.8:3000')
+  const socket = io.connect('https://tracking-devices.herokuapp.com/')
+  let tokenDevice = ''
+
+  useEffect(() => {
+    
+    (async () => {
+      
+      await registerForPushNotificationsAsync().then((token) =>  
+        {setExpoPushToken(token);
+        tokenDevice = token;}
+      );
+      
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      
+      await Location.watchPositionAsync(
+        {
+          // accuracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 0,  
+         },
+        (location) => {
+          setLocation(location)
+          socket.emit('atualizar_dados', location?.coords?.latitude, location?.coords?.longitude, Device.deviceName, tokenDevice)
+        }
+      )
+
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Dispositivo: {Device.deviceName}</Text>
+      <Text>{text}</Text>
+      <Text>Token: {expoPushToken}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +73,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'TRACKING',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
